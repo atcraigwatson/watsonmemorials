b3cbbeae7b465d2a3a08c729efe00f2986b28418
Craig Waton <craig@rokkitpress.com>
2017-04-21 11:16:08 +0100

    Still not working

--- dist/mailer.js
+++ dist/mailer.js
@@ -1,1 +1,27 @@
+$("#contactForm").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
+function submitForm(){
 
+    // Initiate Variables With Form Content
+    var name = $("#contactFormFullName").val();
+    var email = $("#contactFormEmail").val();
+    var tel = $("#contactFormTel").val();
+    var message = $("#contactFormMessage").val();
+
+    $.ajax({
+        type: "POST",
+        url: "mailer.php",
+        data: "name=" + name + "&email=" + email + "&tel=" + tel + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+function formSuccess(){
+    $( "#contactFormMsgSuccess" ).removeClass( "hidden-xs-up" );
+}
--- mailer.php
+++ mailer.php
@@ -1,48 +1,40 @@
 <?php
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["contactFormAntiSpam"] == 9) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        // Get the form fields and remove whitespace.
-        $name    = strip_tags(trim($_POST["contactFormFullName"]));
-		$name    = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email   = filter_var(trim($_POST["contactFormEmail"]), FILTER_SANITIZE_EMAIL);
-        $tel     = strip_tags(trim($_POST["contactFormTel"]));
-        $message = trim($_POST["contactFormMessage"]);
+        $name = $_POST["contactFormFullName"];
+        $email = $_POST["contactFormEmail"];
+        $tel = $_POST["contactFormTel"];
+        $message = $_POST["contactFormMessage"];
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($tel) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-            exit;
-        }
+        $EmailTo = "contact@watsonmemorials.co.uk";
+        $Subject = "New Message Received";
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "craig@watsonmemorials.co.uk";
+        // prepare email body text
+        $Body .= "Name: ";
+        $Body .= $name;
+        $Body .= "\n";
 
-        // Set the email subject.
-        $subject = "Contact form submission from $name";
+        $Body .= "Email: ";
+        $Body .= $email;
+        $Body .= "\n";
 
-        // Build the email content.
-        $email_content  = "Name: $name\n";
-        $email_content .= "Email: $email\n";
-        $email_content .= "Tel: $tel\n\n";
-        $email_content .= "Message:\n$message\n";
+        $Body .= "Tel: ";
+        $Body .= $tel;
+        $Body .= "\n";
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $Body .= "Message: ";
+        $Body .= $message;
+        $Body .= "\n";
 
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+        // send email
+        $success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+        // redirect to success page
+        if ($success){
+           echo "success";
+        }else{
+            echo "invalid";
         }
 
     } else {
