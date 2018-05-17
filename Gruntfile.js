0e523d427b4d360e0f0054e60dbf87a3109beb0a
Craig Waton <craig@rokkitpress.com>
2018-05-17 10:38:04 +0100

    Add Grunt and Local Bootstrap

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Load all Grunt tasks that are listed in package.json automagically
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        exec: {
+            jekyllServe: {
+                cmd: 'bundle exec jekyll serve',
+            },
+            jekyllBuild: {
+                cmd: 'bundle exec jekyll build',
+            },
+        },
+
+        // watch for files to change and run tasks when they do
+        watch: {
+            sass: {
+                files: ['_sass/**/*.scss'],
+                tasks: ['sass']
+            }
+        },
+
+        // compile sass
+        sass: {
+            options: {
+                sourceMap: true,
+            },
+            build: {
+                files: {
+                    'css/style.min.css': '_sass/style.scss'
+                }
+            }
+        },
+
+        postcss: {
+            options: {
+                map: true,
+        
+                processors: [
+                    require('autoprefixer')({
+                        browsers: 'last 2 versions'
+                    }),
+                ]
+            },
+            dist: {
+                src: '_site/css/*.css'
+            }
+        },
+
+        // Minify JS
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'js/scripts.min.js': ['_js/bootstrap.min.js', '_js/arrow-toggle.js', '_js/shortlist.js']
+                }
+            }
+        },
+
+        // Minify images
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/'
+                }]
+            }
+        },
+
+        // run tasks in parallel
+        concurrent: {
+            serve: [
+                'uglify',
+                'sass',
+                'imagemin',
+                'watch',
+                'exec:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+
+    });
+
+    // Register the grunt serve task
+    grunt.registerTask('serve', ['concurrent:serve']);
+
+    // Register the grunt build task
+    grunt.registerTask('build', ['sass', 'exec:jekyllBuild', 'postcss']);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', ['exec:jekyllServe']);
+
+};
