e5b46fbe4674db7ad09970da914e2658e69d7f4e
Craig Waton <craig@watsonmemorials.co.uk>
2024-09-24 09:37:27 +0100

    Format on Save from VScode

--- gruntfile.js
+++ gruntfile.js
@@ -1,85 +1,76 @@
-'use strict';
+"use strict";
 
 module.exports = function (grunt) {
+  const sass = require("node-sass");
 
-    const sass = require('node-sass');
+  // Load all Grunt tasks that are listed in package.json
+  require("load-grunt-tasks")(grunt);
 
-    // Load all Grunt tasks that are listed in package.json automagically
-    require('load-grunt-tasks')(grunt);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
 
-    grunt.initConfig({
+    exec: {
+      jekyllServe: {
+        cmd: "bundle exec jekyll serve",
+      },
+      jekyllBuild: {
+        cmd: "bundle exec jekyll build",
+      },
+    },
 
-        pkg: grunt.file.readJSON('package.json'),
+    // watch for files to change and run tasks when they do
+    watch: {
+      sass: {
+        files: ["_sass/**/*.scss"],
+        tasks: ["sass"],
+      },
+      js: {
+        files: ["_js/*.js"],
+        tasks: ["uglify"],
+      },
+    },
 
-        exec: {
-            jekyllServe: {
-                cmd: 'bundle exec jekyll serve',
-            },
-            jekyllBuild: {
-                cmd: 'bundle exec jekyll build',
-            },
+    // compile sass
+    sass: {
+      options: {
+        implementation: sass,
+        style: "compressed",
+        sourceMap: true,
+      },
+      build: {
+        files: {
+          "css/styles.min.css": "_sass/styles.scss",
         },
+      },
+    },
 
-        // watch for files to change and run tasks when they do
-        watch: {
-            sass: {
-                files: ['_sass/**/*.scss'],
-                tasks: ['sass']
-            },
-            js: {
-                files: ['_js/*.js'],
-                tasks: ['uglify']
-            }
+    // Minify JS
+    uglify: {
+      options: {
+        mangle: false,
+      },
+      my_target: {
+        files: {
+          "js/scripts.min.js": ["_js/bootstrap.min.js", "_js/img-lazy-load.js"],
         },
+      },
+    },
 
-        // compile sass
-        sass: {
-            options: {
-                implementation: sass,
-                style: 'compressed',
-                sourceMap: true
-            },
-            build: {
-                files: {
-                    'css/styles.min.css': '_sass/styles.scss'
-                }
-            }
-        },
+    // run tasks in parallel
+    concurrent: {
+      serve: ["uglify", "sass", "watch", "exec:jekyllServe"],
+      options: {
+        logConcurrentOutput: true,
+      },
+    },
+  });
 
-        // Minify JS
-        uglify: {
-            options: {
-                mangle: false
-            },
-            my_target: {
-                files: {
-                    'js/scripts.min.js': ['_js/bootstrap.min.js', '_js/img-lazy-load.js']
-                }
-            }
-        },
+  // Register the grunt serve task
+  grunt.registerTask("serve", ["concurrent:serve"]);
 
-        // run tasks in parallel
-        concurrent: {
-            serve: [
-                'uglify',
-                'sass',
-                'watch',
-                'exec:jekyllServe'
-            ],
-            options: {
-                logConcurrentOutput: true
-            }
-        },
+  // Register the grunt build task
+  grunt.registerTask("build", ["sass", "exec:jekyllBuild"]);
 
-    });
-
-    // Register the grunt serve task
-    grunt.registerTask('serve', ['concurrent:serve']);
-
-    // Register the grunt build task
-    grunt.registerTask('build', ['sass', 'exec:jekyllBuild']);
-
-    // Register build as the default task fallback
-    grunt.registerTask('default', ['exec:jekyllServe']);
-
+  // Register build as the default task fallback
+  grunt.registerTask("default", ["exec:jekyllServe"]);
 };
